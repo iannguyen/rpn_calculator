57f5681767b2bc14fdc25d0f5eabbb3e2a510b30
Ian Nguyen <iannknguyen@gmail.com>
2016-03-23 13:28:32 -0700

    minor fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
-require_relative '../lib/rpn_calculator'
-
+# Test input helper method
 class RPNCalculator
   def test_input(input)
     valid_input?(input) ? handle_input(input) : invalid_input_prompt
